bf463bfc0bffaed8d0a14b974c3933e4baeb96b3
edensitko <edensit139@gmail.com>
2026-01-29 14:17:35 +0200

    Fix build config for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
-const isExport = process.env.NEXT_PUBLIC_OUTPUT === 'export';
-
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: isExport ? 'export' : 'standalone',
+  output: 'export',
   images: {
-    unoptimized: isExport,
+    unoptimized: true,
   },
 };
 
